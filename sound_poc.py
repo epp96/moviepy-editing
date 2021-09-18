d5353d05034a71083c9936425e6dafdbe1f0a47f
epp96 <epp96@vt.edu>
2021-09-18 17:27:47 -0400

    sound poc

--- sound_poc.py
+++ sound_poc.py
@@ -1,6 +1,11 @@
 # Plays the note A (a sine wave of frequency 440HZ)
 import numpy as np
 from moviepy.editor import *
-make_frame = lambda t: 2*[ np.sin(440 * 2 * np.pi * t) ]
-clip = AudioClip(make_frame, duration=5, fps=15)
-clip.preview()
+make_frame_sin = lambda t: 2*[ np.sin(440 * 2 * np.pi * t) ]
+make_frame_cos = lambda t: 2*[ np.cos(440 * 2 * np.pi * t) ]
+
+clip_sin = AudioClip(make_frame_sin, duration=20, fps=60)
+clip_cos = AudioClip(make_frame_cos, duration=20, fps=60)
+composite_clip = CompositeAudioClip([clip_sin, clip_cos]).set_fps(60)
+# clip = CompositeVideoClip([clip])
+composite_clip.write_audiofile("audio\\temp\\helloworld.wav")
