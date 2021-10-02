4aa2ffec84a345055ee54570fc6f871b37ee6473
epp96 <epp96@vt.edu>
2021-10-02 13:55:10 -0400

    loop_audio function starcted and some more POCs

--- CreateMovie.py
+++ CreateMovie.py
@@ -4,6 +4,7 @@ from os import dup
 from moviepy.editor import *
 import numpy as np
 from PIL import Image
+from loop_audio import loop_audio
 
 
 if len(sys.argv) < 5:
@@ -13,6 +14,7 @@ imagePath = sys.argv[1]
 audioPath = sys.argv[2]
 videoDuration = int(sys.argv[3])
 movieName = sys.argv[4]
+
 print ("resizing image if needed")
 im = Image.open(imagePath)
 imageHeight = im.height if im.height % 2 == 0 else im.height - 1
@@ -24,22 +26,7 @@ print ("image dimensions -> width: " + str(imageWidth) + ", height: " + str(imag
 
 imageclip = ImageClip(imageArray)
 
-
-originalAudio = AudioFileClip(audioPath)
-audio = originalAudio.audio_loop(duration=videoDuration)
-
-patchtimes = []
-timestamp = originalAudio.duration;
-for x in range(int(videoDuration/originalAudio.duration)): 
-    patchtimes.append(timestamp - 1)
-    timestamp+=originalAudio.duration
-
-print ("time stamps are " + str([str(datetime.timedelta(seconds = patchtime)) for patchtime in patchtimes]))
-audioList = [audio]
-if (len(patchtimes) > 0):
-    patchesClip = CompositeAudioClip([originalAudio.set_start(timestamp).set_end(timestamp + 3) for timestamp in patchtimes]).set_duration(videoDuration)
-    audioList.append(patchesClip)
-audio = CompositeAudioClip(audioList)
+audio = loop_audio(videoDuration, audioPath)
 imageclip = imageclip.set_audio(audio).set_duration(audio.duration).volumex(5)
 
 
