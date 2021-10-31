0683eaed3a3de4cf292293425a7ef2159f625558
epp96 <epp96@vt.edu>
2021-10-31 18:34:02 -0400

    make loopable function poc it is pretty slow and more testing needs be done

--- backwards_poc.py
+++ backwards_poc.py
@@ -6,21 +6,29 @@ import os
 
 from loop_audio import loop_audio
 
-videoDuration = 3600
+videoDuration = 60 * 6
 
-clip = VideoFileClip("movies\\practice\\windy-autum.mp4")
 # some times i get an error that says it cannot read the first (i think it means last) frame of the video
 # internet solution is to take out one sec at the end
-audio = loop_audio(videoDuration, audioClip=clip.audio)
+clip = VideoFileClip("movies\\pexels-tima-miroshnichenko-6316263winter.mp4")
+clip = clip.subclip(0, 10)
 
-clip = clip.subclip(0, clip.duration - 1)
+audio = loop_audio(videoDuration, audioPath="audio\\Night Snow Asher Fulero.mp3")
 
-clip = vfx.time_symmetrize(clip)
+# clip = vfx.time_symmetrize(clip)
 
+final = vfx.make_loopable(cross=3, clip=clip)
+# final = vfx.fadein(clip=final, duration=2, initial_color=None)
 final = vfx.loop(clip, duration=videoDuration).set_audio(audio)
 
-clip_name = "movies\\windy-autum" + str(datetime.datetime.timestamp(datetime.datetime.now())) + ".mp4"
 
-final.write_videofile(clip_name)
+clip_name = "movies\\makeLoopable" + str(datetime.datetime.timestamp(datetime.datetime.now())) + ".mp4"
 
-# os.system("start " + clip_name)
+# final.write_videofile(clip_name)
+
+final.write_videofile(clip_name, threads=256, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac", logger="bar")
+command = "open"
+if sys.platform == 'win32':
+    command = "start"
+
+os.system(command + " " + clip_name)
--- fade_poc.py
+++ fade_poc.py
@@ -4,50 +4,61 @@ from moviepy.editor import *
 import moviepy.video.fx.all as vfx
 import moviepy.audio.fx.all as afx
 import numpy as np
+import sys
 
 from loop_audio import loop_audio
   
-videoDuration = 60 * 60 * 3
+videoDuration = 60 * 1
 
 verbose = True
 # clip_rain = VideoFileClip("movies/overlays/pexels-atypeek-dgn-7043616.mp4")
-clip_rain = VideoFileClip("movies/overlays/pexels-gusat-andrei-6527130.mp4")
-clip_rain = vfx.speedx(clip_rain, 50)
-clip_rain = vfx.loop(clip_rain, duration=videoDuration) # like this
-clip_rain = clip_rain.set_opacity(.50)
+# clip_rain = vfx.speedx(clip_rain, 50)
+clip_rain = VideoFileClip("movies/overlays/rain.mp4")
 
 # TODO: test this audio looping method ,ay be much faster than what we have 
 # make sure the it doesnt have the silent gaps between loops
 # audio = afx.audio_loop( audioClip, duration=videoDuration)
+backgroun_path = "pictures/pexels-pixabay-371662.jpg"
+# background = VideoFileClip(backgroun_path).set_audio(None)
+# # background = background.subclip(0, 9).set_audio(None)
 
-# background = VideoFileClip("movies/production ID_4154555.mp4")
-# background = background.subclip(0, 9)
-
-# background = vfx.time_symmetrize(background)
-# background = vfx.loop(background, duration=videoDuration).set_audio(None)
+# # background = vfx.time_symmetrize(background)
+# background = vfx.loop(background, duration=videoDuration)
 
 
 
-imagePath = "pictures/pexels-jan-kopřiva-3634855.jpeg";
-image = Image.open(imagePath)
+# imagePath = "pictures/pexels-jan-kopřiva-3634855.jpeg";
+image = Image.open(backgroun_path)
 im = image.resize(clip_rain.size)
 imageArray = np.array(im)
 
 background = ImageClip(imageArray)
 
-audioPath = "audio/543449__kostas17__howling-wind.wav"
+audioPath = "audio/Light Rain.mp3"
 audio = loop_audio(videoDuration, audioPath=audioPath)
-clip_rain = clip_rain.set_audio(audio).volumex(5)
-final = CompositeVideoClip([background, clip_rain], size=clip_rain.size).set_duration(videoDuration)
-
+alarm_path = "audio/219244__zyrytsounds__alarm-clock-short.wav"
+alarm = AudioFileClip(alarm_path)
+alarm = alarm.set_start(videoDuration - alarm.duration)
+audio = CompositeAudioClip([audio, alarm]).volumex(5)
+# clip_rain = clip_rain.set_audio(audio)
+# final = CompositeVideoClip([background], size=background.size).set_duration(audio.duration).set_audio(audio)
+# final = concatenate_videoclips([final,alarm])
+clip_rain = vfx.loop(clip_rain, duration=audio.duration) # like this
+clip_rain = clip_rain.set_opacity(.50)
+final = CompositeVideoClip([background, clip_rain], size=clip_rain.size).set_duration(audio.duration).set_audio(audio)
+final = vfx.fadein(clip=final, duration=2, initial_color=None)
 today = datetime.date.today()
-final_clip_path = "movies/windhouling" + str(today) + ".mp4"
+final_clip_path = "movies/bonfire" + str(today) + ".mp4"
 # showing final clip
 # final.write_videofile(final_clip_path, verbose=False, logger=None)
 logger = logger="bar" if verbose else None
-final.write_videofile(final_clip_path, threads=32, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac", logger="bar" if verbose else None)
+final.write_videofile(final_clip_path, threads=64, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac", logger="bar" if verbose else None)
+command = "open"
+if sys.platform == 'win32':
+    command = "start"
 
-command = "open " + final_clip_path
+
+command = command + " " + final_clip_path
 print(command)
 os.system(command=command)
 
